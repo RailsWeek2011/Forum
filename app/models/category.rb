97b2c5ccd1d534dbfbfbb8d88b58e51d31b8d7e1
Florian Thomas <florianthomas90@gmail.com>
2011-07-22 09:22:35 +0200

    Validation added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   belongs_to :category
   has_many :freds
+  
+  validates :name, :presence => true, :length => {:minimum => 3, :maximum => 70}, :uniqueness => true
+  
 end
--- app/models/fred.rb
+++ app/models/fred.rb
@@ -1,3 +1,5 @@
 class Fred < ActiveRecord::Base
   belongs_to :category
+  
+  validates :headline, :presence => true, :length => {:minimum => 3, :maximum => 40}
 end
