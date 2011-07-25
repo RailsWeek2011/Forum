99fc5d3d0aaf7ec865d25918d50946f4792e1bae
Florian Thomas <florianthomas90@gmail.com>
2011-07-25 14:55:02 +0200

    bug in view and routing fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :set_locale
+  
+  check_authorization   :unless => :devise_controller?
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -11,9 +16,5 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
-  check_authorization   :unless => :devise_controller?
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-    end
   end
 end
