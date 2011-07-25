ab321c30722063434b2117a859daf0d9845f6cd0
Hagen Lauer <hagen.lauer@gmail.com>
2011-07-25 14:33:06 +0200

    refactoring

--- app/helpers/profiles_helper.rb
+++
@@ -1,9 +0,0 @@
-module ProfilesHelper
-  def proflieExists?
-    if @Profiles.find current_user.id != nil
-      return true
-    else
-      return false
-    end
-  end
-end
--- app/models/profile.rb
+++
@@ -1,5 +0,0 @@
-class Profile < ActiveRecord::Base
-  belongs_to :user
-  validates :nick, :presence => true, :length => {:minimum => 3, :maximum => 40}, :uniqueness => true
-  validates :user_id, :uniqueness => true
-end
