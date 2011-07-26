cb562357e929b399233b6c24f440dad09b2a3bb9
Hagen Lauer <hagen.lauer@gmail.com>
2011-07-26 13:27:36 +0200

    bugfixes etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   load_and_authorize_resource
+  check_authorization
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+
   def index
     @users = User.all
 
@@ -29,6 +35,20 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html {redirect_to show_users_path}
       format.json { head :ok }
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, :notice => 'User was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
