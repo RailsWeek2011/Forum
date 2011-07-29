934acfd5962de83458d0413da3356a79eba0e605
Hagen Lauer <hagen.lauer@gmail.com>
2011-07-29 10:16:21 +0200

    timefix, uml added

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -38,5 +39,7 @@ module Forum
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    config.time_zone = 'Berlin'
   end
 end
